c6ab88231328d1bb2fca76caa597cf2f042f2778
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-08-29 07:30:33 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Carton</h1>
+        <nav>
+          <button>Products</button>
+          <button>Cart</button>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
