9c9ee76d4f4e1026c5c50a359ea2d1d1e560ac7a
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-08-30 19:32:07 +0530

    feat: add icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import useFetch from "./hooks/useFetch";
 import { useState } from "react";
+import { Handbag } from "lucide-react";
 
 function App() {
   const [products, error, loading] = useFetch();
@@ -13,18 +14,18 @@ function App() {
           <Link to={"/"}>CartOn</Link>
         </h1>
         <nav>
-          <button>
-            <Link to={"/products"}>Products</Link>
-          </button>
-          <button>
-            <Link to={"/cart"}>Cart</Link>
-          </button>
+          <Link to={"/products"}>Products</Link>
+          <Link to={"/cart"}>
+            <Handbag />
+          </Link>
         </nav>
       </header>
       <hr />
-      <Outlet
-        context={[cartItems, setCartItems, products, error, loading]}
-      ></Outlet>
+      <main>
+        <Outlet
+          context={[cartItems, setCartItems, products, error, loading]}
+        ></Outlet>
+      </main>
       <hr />
       <footer>Made with ❤️ by Nandkishor</footer>
     </>
